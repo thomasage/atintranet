93aeab03d43871bd5f9b988ccc2c4f63936b9e35
Thomas Agé <thomas@thomasage.fr>
2019-03-02 09:06:00 +0100

    Fix add detail

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -14,9 +14,10 @@ $(function () {
             InvoiceApp.$detailsContainer.data('index', index + 1);
             InvoiceApp.$detailsContainer.append($element);
             scrollToElement($element);
+            $element.find(':input').eq(0).focus();
         },
         handleDetailsLinkCopy: function (e) {
-            let $row = $(e.target).closest('tr');
+            let $row = $(e.target).closest('div[id]');
             let index = InvoiceApp.$detailsContainer.data('index');
             let element = InvoiceApp.$detailsContainer.data('prototype');
             element = element.replace(/__name__label__/g, index);
@@ -30,6 +31,7 @@ $(function () {
             InvoiceApp.$detailsContainer.data('index', index + 1);
             InvoiceApp.$detailsContainer.append($element);
             scrollToElement($element);
+            $element.find(':input').eq(0).focus();
         },
         handleDetailsLinkDelete: function (e) {
             $(e.target).closest('div[id]').slideUp('normal', function () {
@@ -38,7 +40,6 @@ $(function () {
         },
         initialize: function ($detailsContainer, $detailsLinkAdd) {
             this.$detailsContainer = $detailsContainer;
-            this.$detailsContainer.data('index', this.$detailsContainer.find('tr').length);
             this.$detailsContainer.find('.js-detail-copy').on('click', this.handleDetailsLinkCopy);
             this.$detailsContainer.find('.js-detail-delete').on('click', this.handleDetailsLinkDelete);
             this.$detailsLinkAdd = $detailsLinkAdd;
