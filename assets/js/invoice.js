49b3dc4c079b723e3be64607abdf6e266cfafeed
Thomas Agé <thomas@thomasage.fr>
2019-03-30 09:43:48 +0100

    Add update amounts on invoice form

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -2,6 +2,8 @@ let $ = require('jquery');
 
 $(function () {
 
+    let lang = $('html').prop('lang');
+
     let InvoiceApp = {
         handleDetailsLinkAdd: function () {
             let index = InvoiceApp.$detailsContainer.data('index');
@@ -38,12 +40,42 @@ $(function () {
                 $(this).remove();
             });
         },
+        handleDetailsUpdateAmounts: function (element) {
+            let amount = InvoiceApp.moneyUnFormat(element.find('[id$="_amountUnit"]').val());
+            let quantity = InvoiceApp.moneyUnFormat(element.find('[id$="_quantity"]').val());
+            element.find('[id$="_amountTotal"]').val(InvoiceApp.moneyFormat(amount * quantity));
+            this.handleUpdateAmounts();
+        },
+        handleUpdateAmounts: function () {
+            let amount = 0.0;
+            this.$detailsContainer.find('> div').each(function () {
+                amount += InvoiceApp.moneyUnFormat($(this).find('[id$="_amountTotal"]').val());
+            });
+            $('#invoice_amountExcludingTax').val(InvoiceApp.moneyFormat(amount));
+            let taxRate = InvoiceApp.moneyUnFormat($('#invoice_taxRate').val());
+            let taxAmount = amount * taxRate / 100;
+            $('#invoice_taxAmount').val(InvoiceApp.moneyFormat(taxAmount));
+            $('#invoice_amountIncludingTax').val(InvoiceApp.moneyFormat(amount + taxAmount));
+        },
         initialize: function ($detailsContainer, $detailsLinkAdd) {
             this.$detailsContainer = $detailsContainer;
             this.$detailsContainer.find('.js-detail-copy').on('click', this.handleDetailsLinkCopy);
             this.$detailsContainer.find('.js-detail-delete').on('click', this.handleDetailsLinkDelete);
             this.$detailsLinkAdd = $detailsLinkAdd;
             this.$detailsLinkAdd.on('click', this.handleDetailsLinkAdd);
+            this.$detailsContainer.find('input').on('change', function () {
+                InvoiceApp.handleDetailsUpdateAmounts($(this).closest('div[id^="invoice_details_"]'));
+            });
+        },
+        moneyFormat: function (string) {
+            return string.toLocaleString(lang, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+        },
+        moneyUnFormat: function (string) {
+            var parts = (1234.5).toLocaleString(lang).match(/(\D+)/g);
+            var unformatted = string;
+            unformatted = unformatted.split(parts[0]).join("");
+            unformatted = unformatted.split(parts[1]).join(".");
+            return parseFloat(unformatted);
         }
     };
 
